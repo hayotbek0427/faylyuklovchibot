d3b0ba7aed473f1468b0d0f6ef0cb480f96cdaba
hayotbek0427 <67702526+hayotbek0427@users.noreply.github.com>
2021-01-28 02:45:00 -0600

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,83 +1,81 @@
 class Translation(object):
-    START_TEXT = """Hello,
-This is a Telegram URL Upload Bot!
+    START_TEXT = """Salomm,
+Bu Telegram URL manzilini yuklash botidir!
+<b>Iltimos, menga to'g'ridan-to'g'ri yuklab olish uchun URL manzilini yuboring, men Telegram-ga Fayl / Video sifatida yuklashim mumkin </b>
 
-<b>Please send me any direct download URL Link, i can upload to telegram as File/Video</b>
+/help batafsil ma'lumot uchun ..
 
-/help for more details..
-
-Support Group : @InFoTelGroup
-© @SpEcHlDe , @TGBotsZ & @CWProjects"""
-    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
-    ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
-    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
-URL | filename | username | password"""
-    NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
-    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @TGBotsZ"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @TGBotsZ \nUploaded in {} seconds."
-    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+Yordam guruhi : @wordpress_uzgroup
+© @excellend_boy """
+    RENAME_403_ERR = "Kechirasiz. Sizga ushbu fayl nomini o'zgartirishga ruxsat berilmagan."
+    ABS_TEXT = " Iltimos, xudbin bo'lmang."
+    UPGRADE_TEXT = "<b>👉 botdan foydalanish uchun havola tashlashingiz mumkin </b>  /help Tafsilotlar uchun"
+    FORMAT_SELECTION = "Kerakli formatni tanlang: <a href='{}'>fayl hajmi taxminiy bo'lishi mumkin </a> \nAgar siz maxsus eskizni o'rnatmoqchi bo'lsangiz, quyidagi tugmachalardan birini bosgandan oldin yoki tezda fotosuratni yuboring. \ NAvtomobil ravishda yaratilgan eskizni o'chirish uchun / o'chirish rasmini ishlatishingiz mumkin."
+    SET_CUSTOM_USERNAME_PASSWORD = """Agar siz premium videolarni yuklab olishni istasangiz, quyidagi formatda taqdim eting:
+URL | Fayl nomi | foydalanuvchi nomi | parol"""
+    NOYES_URL = "@robot URL aniqlandi. Iltimos, foydalaning https://shrtz.me/PtsVnf6 va tezkor URL-manzilni oling, shunda men Telegram-ga yuklashim mumkin, boshqa foydalanuvchilar uchun sekinlashmasdan."
+    DOWNLOAD_START = "yuklab olishga harakat qilmoqda"
+    UPLOAD_START = "yuklashga harakat qilmoqda"
+    RCHD_BOT_API_LIMIT = "ruxsat etilgan maksimal kattalikdan kattaroq (50MB). Shunga qaramay, yuklashga harakat qilmoqda."
+    RCHD_TG_API_LIMIT = "Yuklandi {} soniya.\nAniqlangan fayl hajmi: {}\nKechirasiz. Ammo, Telegram API cheklovlari tufayli 2 Gb dan katta fayllarni yuklay olmayman."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Iltimos, menga foydali deb topsangiz, menga baho bering. Qo'shiling : @wordpress_uzb"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Yuklandi  {} soniya. \nQo'shiling : @wordpress_uzb \nYuklandi {} soniya."
+    NOT_AUTH_USER_TEXT = "Iltimos /upgrade sizning obunangiz."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Aniqlangan fayl hajmi: {}. Bepul foydalanuvchilar faqatgina quyidagilarni yuklashlari mumkin: {}\nIltimos / obunangizni yangilang.\nAgar bu xato deb hisoblasangiz, iltimos, murojaat qiling <a href='https://telegram.dog/ThankTelegram'>@wordpress_uzb</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Maxsus video / fayl eskizi saqlandi. Ushbu rasm videoda ishlatiladi / file."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Maxsus eskiz muvaffaqiyatli o'chirildi."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media muvaffaqiyatli tozalandi."
+    SAVED_RECVD_DOC_FILE = "Hujjat muvaffaqiyatli yuklab olindi."
     CUSTOM_CAPTION_UL_FILE = " "
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "Maxsus ThumbNail topilmadi."
+    NO_VOID_FORMAT_FOUND = "XATO...\n<b>YouTubeDL</b> dedi: {}"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    CURENT_PLAN_DETAILS = """Current plan details
+    CURENT_PLAN_DETAILS = """Joriy reja tafsilotlari
 --------
 Telegram ID: <code>{}</code>
-Plan name: Free Cloned User
-Expires on: 31/12/2020"""
-    HELP_USER = """Hai am URL Uploader bot..
-    
-1. Send url (Link|New Name with Extension).
-2. Send Custom Thumbnail (Optional).
-3. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File with Screenshots
-   Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
+Reja nomi: Bepul klonlangan foydalanuvchi
+Muddati tugaydi: 31/12/2030"""
+    HELP_USER = """Salom, URL yuklovchi bot ..
+1. URL yuboring (Havola | Kengaytirilgan yangi ism).
+2. Maxsus eskizni yuboring (ixtiyoriy).
+3. Tugmani tanlang.
+   SVideo - Faylni skrinshotlar bilan video sifatida bering
+   DFile - skrinshotlar bilan fayl bering
+   Video - Faylni skrinshotlarsiz video sifatida bering
+   DFile - Faylni skrinshotlarsiz bering
    
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/QkAkSLBgoYw">Diploy</a>
+<b>👉 O'zingizning biznesingiz ni boshlamoqchimisiz unda  :</b> 👉 <a href="https://t.me/afexuz">kanalimizga ulaning</a>
 
 --------
-Send /me to know current plan details
+yuboring /me menga rejaning joriy tafsilotlarini bilish uchun yuboring
 
-Support Group : @InFoTelGroup
-© @TGBotsZ"""
-    REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
-    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
-    REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
-    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
-    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @AnyDLBot"
+Yordam guruhi : @wordpress_uzgroup
+© @wordpress_uzb"""
+    REPLY_TO_DOC_GET_LINK = "Yuqori tezlikdagi to'g'ridan-to'g'ri yuklab olish havolasini olish uchun Telegram-ga xabar yuboring"
+    REPLY_TO_DOC_FOR_C2V = "Konvertatsiya qilish uchun Telegram-ga javob bering"
+    REPLY_TO_DOC_FOR_SCSS = "Skrinshotlarni olish uchun Telegram-dagi ommaviy axborot vositalariga javob bering"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Telegram-ga maxsus eskizni qo'llab-quvvatlash bilan nomini o'zgartirish / o'zgartirish uchun javob bering"
+    AFTER_GET_DL_LINK = "To'g'ridan-to'g'ri bog'lanish <a href='{}'>Yaratilgan</a> valid for {} days.\n© @AnyDLBot"
     FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
-    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
+    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "Yaratilgan /downloadmedia har qanday ommaviy axborot vositasiga yuklab oling, shunda uni mahalliy tilimga yuklab olishim mumkin. \nYuborish /storageinfo hozirda yuklab olinayotgan ommaviy axborot vositalarini bilish."
     FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
-    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "A saved media already exists. Please send /storageinfo to know the current media details."
-    USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> deleted from DataBase."
-    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a Telegram media (MKV), to extract embedded streams"
-    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
-    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
-    INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
-    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "Saqlangan media allaqachon mavjud. Iltimos, joriy media ma'lumotlarini bilish uchun / storageinfo-ni yuboring."
+    USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> DataBase-dan o'chirildi."
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "O'rnatilgan oqimlarni chiqarish uchun Telegram-ga (MKV) javob bering"
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Javob /generatecustomthumbnail maxsus eskiz yaratish uchun media albomga"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media albomda faqat ikkita rasm bo'lishi kerak. Iltimos, media albomni qayta yuboring, so'ngra qayta urinib ko'ring yoki albomga faqat ikkita rasm yuboring."
+    INVALID_UPLOAD_BOT_URL_FORMAT = "URL formati noto'g'ri. urlingiz ikkalasidan ham boshlanishiga ishonch hosil qiling http:// yoki https://.Format havolasi yordamida maxsus fayl nomini o'rnatishingiz mumkin | file_name.extension"
+    ABUSIVE_USERS = "Sizga ushbu botdan foydalanishga ruxsat berilmagan. Agar buni xato deb hisoblasangiz, iltimos, ushbu cheklovni olib tashlash uchun / meni tekshiring."
     FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/FFMpegRoBot"
-    EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
-    EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
+    EXTRACT_ZIP_INTRO_ONE = "Avval siqilgan faylni yuboring, Keyin javob bering /unzip faylga buyruq."
+    EXTRACT_ZIP_INTRO_THREE = "Qabul qilingan faylni tahlil qilish. ⚠️ Bu biroz vaqt talab qilishi mumkin. Iltimos, sabr qiling. "
     UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
-    EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
-You can use /rename command after receiving file to rename it with custom thumbnail support."""
-    CANCEL_STR = "Process Cancelled"
-    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
-    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
-Free users only 1 request per 30 minutes.
-/upgrade or Try 1800 seconds later."""
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    EXTRACT_ZIP_ERRS_OCCURED = "Kechirasiz. Siqilgan faylni qayta ishlash paytida xatolar yuz berdi. Iltimos, barchasini yana ikki marta tekshiring va agar muammo hal etilmasa, bu haqda xabar bering <a href='https://telegram.dog/wordpress_uzgroup'>@wordpress_uzgroup</a>"
+    EXTRACT_ZIP_STEP_TWO = """Tanlang file_name quyidagi variantlardan yuklash uchun.
+Faylni olganingizdan so'ng uni nomini o'zgartirish uchun buyruqni foydalanishingiz mumkin."""
+    CANCEL_STR = "Jarayon bekor qilindi"
+    ZIP_UPLOADED_STR = "Yuklandi {} fayllar {} soniya"
+    FREE_USER_LIMIT_Q_SZE = """Qayta ishlash mumkin emas.
+Bepul foydalanuvchilar 30 daqiqada atigi 1 ta so'rov yuborishadi.
+/upgrade yoki 1800 soniyadan keyin harakat qilib ko'ring."""
+    SLOW_URL_DECED = "Bu juda sekin URL kabi ko'rinadi. Siz mening uyimni buzib tashlaganingiz uchun, men ushbu faylni yuklab olishga kayfiyatim yo'q. Ayni paytda, nega buni sinab ko'rmaysiz: ==> https://shrtz.me/PtsVnf6 va menga tezkor URL manzilini bering, shunda men Telegram-ga yuklashim mumkin, boshqa foydalanuvchilar uchun sekinlashmasdan."
